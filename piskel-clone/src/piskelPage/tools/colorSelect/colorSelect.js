cd9c1880af3e7a59c0bf1c479bb1267bfccfb0ef
Yauhen Kviatkouski <kvetkovski@gmail.com>
2019-06-29 10:29:43 +0300

    feat: add ability to change colors

---
+++ piskel-clone/src/piskelPage/tools/colorSelect/colorSelect.js
@@ -0,0 +1,21 @@
+import './colorSelect.css';
+import { createElement } from '../../../utilites/common-functions';
+
+export default function colorSelect() {
+  const colorBlock = createElement('div', 'color-block');
+  document.querySelector('.tools').lastChild.insertAdjacentElement('afterend', colorBlock);
+  const inputSecondary = createElement('input', 'color-secondary');
+  colorBlock.insertAdjacentElement('afterbegin', inputSecondary);
+  inputSecondary.type = 'color';
+  inputSecondary.value = 'black';
+  const inputPrimary = createElement('input', 'color-primary');
+  colorBlock.insertAdjacentElement('beforeend', inputPrimary);
+  inputPrimary.type = 'color';
+  inputPrimary.value = 'black';
+  inputPrimary.addEventListener('input', (colorPrimary) => {
+    window.state.color1 = colorPrimary.srcElement.value;
+  });
+  inputPrimary.addEventListener('input', (colorSecondary) => {
+    window.state.color2 = colorSecondary.srcElement.value;
+  });
+}
--- piskel-clone/src/piskelPage/tools/tools.js
+++ piskel-clone/src/piskelPage/tools/tools.js
@@ -2,9 +2,11 @@ import './tools.css';
 import { createElement } from '../../utilites/common-functions';
 import { penButton } from './penAndEraser/penAndEraser';
 import eraserButton from './penAndEraser/eraser';
+import colorSelect from './colorSelect/colorSelect';
 
 export default function tools() {
   document.body.insertAdjacentElement('beforeend', createElement('div', 'tools'));
   penButton();
   eraserButton();
+  colorSelect();
 }
